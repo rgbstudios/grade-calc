2b52490ade843042bc36a1e8a59b85d4d62c489a
Justin Golden <justingolden21@gmail.com>
2020-05-06 04:36:18 -0700

    add download csv option (alpha)

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,8 @@ $( ()=> {
 		$('#console').focus();
 	});
 
+	$('#speadsheet-btn').click(downloadCSV);
+
 	$('#night-btn').click( ()=> {
 		$('#nightTheme').attr('href', $('#nightTheme').attr('href')?'':'css/night.css');
 	});
@@ -92,15 +94,21 @@ function doCalc() {
 	let grade = 0;
 	invalid = false;	
 	let message = '';
+
+	let gradeHistoryData = [$('#student-name').val()];
 	$('.grade-item').each( (idx, val)=> {
 		let item = getAssignmentGrade($(val), weightTotal);
 		if(item.invalid) {
 			invalid = true;
 			message = item.message;
 		 } else {
-			grade += item.grade;		 	
+			grade += item.grade;
+
+			gradeHistoryData.push($(val).find('.score').val() );
 		}
 	});
+	gradeHistoryData.push(grade);
+
 	if(invalid) {
 		$('#letter-text').html('');
 		$('#grade-text').html(message);
@@ -112,6 +120,8 @@ function doCalc() {
 		$('#grade-text').html(grade + '%');
 	
 		$('.grade-item:first-child').find('.score').select();
+
+		addGradeHistory(gradeHistoryData);
 	}
 }
 
@@ -195,4 +205,73 @@ function copyText(str) {
 	tmp.select();
 	document.execCommand('copy');
 	tmp.remove();
+}
+
+// ----------------
+
+let gradeHistory = [];
+
+// add student name, then scores on each assignment
+function addGradeHistory(grades) {
+	gradeHistory.push(grades);
+}
+
+// note: doesn't error check for assignments deleted inbetween, invalid numbers, etc
+// https://stackoverflow.com/a/14966131/4907950
+function downloadCSV() {
+
+	// assignment labels
+	let rows = [
+		['Assignment Name', 'Assignment Total', 'Assignment Weight']
+	];
+
+	let assignmentNames = [];
+
+	// assignments
+	$('.grade-item').each( (idx, val)=> {
+		// $(val).find('.score')
+		rows.push([
+			$(val).find('.name').val(),
+			$(val).find('.total').val(),
+			$(val).find('.weight').val()
+		]);
+
+		let assignmentName = $(val).find('.name').val();
+		assignmentName = assignmentName==''?'Untitled Assignment':assignmentName;
+		assignmentNames.push(assignmentName + ' Grade');
+	});
+
+	// empty row before student data
+	rows.push([]);
+
+	assignmentNames.push('Final Grade');
+	assignmentNames.unshift('Student Name');
+	// student grades label
+	// unshift adds to beginning of array
+	rows.push(assignmentNames );
+
+	// add student data
+	// 2d array, pushing in each 1d array
+	gradeHistory.forEach( val=> rows.push(val) );
+
+
+	// const rows = [
+	// 	['name1', 'city1', 'some other info'],
+	// 	['name2', 'city2', 'more info']
+	// ];
+
+	console.log(assignmentNames);
+	console.log(gradeHistory);
+	console.log(rows);
+
+	let csvContent = 'data:text/csv;charset=utf-8,' 
+		+ rows.map(e => e.join(',') ).join('\n');
+
+	let encodedUri = encodeURI(csvContent);
+
+	let link = document.createElement('a');
+	link.setAttribute('href', encodedUri);
+	link.setAttribute('download', 'grade_data.csv');
+	document.body.appendChild(link); // for firefox
+	link.click();
 }
