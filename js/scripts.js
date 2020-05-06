d5027c0b0d4c183a0ff8e0383a2679d1c44d4dc3
Justin Golden <justingolden21@gmail.com>
2020-05-06 16:48:47 -0700

    cleaning js, add remove all btn, css updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,5 @@
 let invalid;
 
-$( ()=> {
-
-	$('[data-toggle="popover"]').popover({trigger:'hover', placement:'bottom'});
-
-	$('#speadsheet-btn').click(downloadCSV);
-
-	$('#night-btn').click( ()=> {
-		$('#nightTheme').attr('href', $('#nightTheme').attr('href')?'':'css/night.css');
-	});
-
-	for(let i=0; i<3; i++) {
-		makeNewDiv();
-	}
-
-	$('#add-btn').click(makeNewDiv);
-
-	$('#clear-btn').click( ()=> {
-		$('.score').val(0);
-		$('#student-name').val('');
-	});
-
-	$('#calc-btn').click(doCalc);
-	$('#fullscreen-btn').click(toggleFullscreen);
-
-	$('#same-btn').click( ()=> {
-		let weightInputs = $('.weight');
-		for(input of weightInputs)
-			input.value = 100/weightInputs.length;
-	});
-
-	$('#copy-btn').click( ()=> {
-		if(!invalid) {
-			copyText($('#grade-text').html() + ' ' + $('#letter-text').html() );
-			//todo: notificaiton it was copied
-		}
-	});
-
-	$('#clear-console-btn').click( ()=> $('#console').val('') );
-	
-	$('#download-console-btn').click( ()=> {		
-		let data = [($('#console').val().replace(/\r?\n/g, '\r\n'))];		
-		let properties = {type: 'plain/text'};
-		try {
-			file = new File(data, 'grades.txt', properties);
-		} catch(e) {
-			file = new Blob(data, properties);
-		}
-		$('#download-link').prop('download', 'grade console ' + getFormattedDate() + '.txt');
-		$('#download-link').prop('href', URL.createObjectURL(file) );
-	});
-
-});
-
-function makeNewDiv() {
-	$('#grade-items').append(
-		'<div class="grade-item">' + 
-			'<span class="grade-label">Score: &nbsp;</span>' +
-			'<input type="number" min="0" class="score form-control input-sm" value="0" title="Score">' +
-			'<span>&nbsp;/&nbsp;</span>' +
-			'<input type="number" min="0" value="100" class="total form-control input-sm" tabIndex="-1" title="Total">' +
-			'<span class="breakP">&nbsp;|&nbsp;</span>' +
-			'<span class="grade-label">Weight: &nbsp;</span>' +
-			'<input type="number" min="0" class="weight form-control input-sm" tabIndex="-1" title="Weight (should add to 100%)">' +
-			'<span> %</span>' +
-			'<span class="breakP">&nbsp;|&nbsp;</span>' +
-			'<span class="grade-label">Name: &nbsp;</span>' +
-			'<input type="text" class="name form-control input-sm" tabIndex="-1" title="Assignment Name (optional)" placeholder="Assignment (optional)">' +
-			'<button class="btn delete-btn" title="Delete Assignment" tabIndex="-1" onclick="this.parentNode.parentNode.removeChild(this.parentNode);">' +
-			'<i class="fas fa-times"></i></button>' +
-			'<small><span class="grade-info"></span></small>' +
-		'</div>'
-	);
-}
-
 function doCalc() {
 	if($('.grade-item').length==0) {
 		invalid = true;
@@ -253,10 +179,6 @@ function downloadCSV() {
 	// 	['name1', 'city1', 'some other info'],
 	// 	['name2', 'city2', 'more info']
 	// ];
-
-	console.log(assignmentNames);
-	console.log(gradeHistory);
-	console.log(rows);
 
 	let csvContent = 'data:text/csv;charset=utf-8,' 
 		+ rows.map(e => e.join(',') ).join('\n');
